74b9773b784cda58d24acdd020b192ad249d9ec9
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-08-21 19:14:04 +0200

    Reset cursor after clicking

--- src/lib/components/Trafficlight.svelte
+++ src/lib/components/Trafficlight.svelte
@@ -4,16 +4,15 @@
     class="h-3 w-3 rounded-full bg-white/20 transition-all duration-200 hover:bg-[#FF5F57]"
     on:click={(e) => {
       // give the element classes
-      e.target.classList.add('scale-150', 'opacity-0');
+      e.target.classList.add('scale-150', 'opacity-0', 'cursor-default');
       setTimeout(() => {
         e.target.classList.add('scale-0');
         e.target.classList.remove('scale-150');
       }, 300);
       setTimeout(() => {
-        e.target.classList.remove('scale-0', 'opacity-0', 'scale-150');
+        e.target.classList.remove('scale-0', 'opacity-0', 'scale-150', 'cursor-default');
       }, 5000);
-    }}
-  />
+    }} />
   <button
     type="button"
     class="h-3 w-3 rounded-full bg-white/20 transition-all duration-200 hover:bg-[#FDBB2E]"
@@ -27,8 +26,7 @@
       setTimeout(() => {
         e.target.classList.remove('scale-0', 'opacity-0', 'scale-150');
       }, 5000);
-    }}
-  />
+    }} />
   <button
     class="h-3 w-3 rounded-full bg-white/20 transition-all duration-200 hover:bg-[#27C840]"
     on:click={(e) => {
@@ -41,6 +39,5 @@
       setTimeout(() => {
         e.target.classList.remove('scale-0', 'opacity-0', 'scale-150');
       }, 5000);
-    }}
-  />
+    }} />
 </div>
