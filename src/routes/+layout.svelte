9742f5a2c5925efe7e09740154dac673e4014cc3
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-10-20 16:13:01 +0200

    Add Vercel Analytics - Speed Insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,20 @@
 <script>
-  import { dev } from '$app/environment';
+  import { browser, dev } from '$app/environment';
+  import { page } from '$app/stores';
+  import { webVitals } from '$lib/vitals';
   import { inject } from '@vercel/analytics';
-  inject({ mode: dev ? 'development' : 'production' });
   import '../app.postcss';
+  inject({ mode: dev ? 'development' : 'production' });
+
+  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+  $: if (browser && analyticsId) {
+    webVitals({
+      path: $page.url.pathname,
+      params: $page.params,
+      analyticsId
+    });
+  }
 </script>
 
 <slot />
