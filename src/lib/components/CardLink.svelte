21d7ed0d896160295fef569141bb7be243495417
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-08 10:55:35 +0100

    Add CardLink component with href and target props.

---
+++ src/lib/components/CardLink.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let href: string;
+  export let target = '_blank';
+</script>
+
+<a {href} {target} class={`relative m-0 cursor-pointer overflow-hidden rounded-none border-y-2  border-neutral-700/40 p-0 transition-all duration-300 md:rounded-3xl md:border-2 lg:hover:scale-105 ${$$props.class}`}>
+  <slot />
+</a>
