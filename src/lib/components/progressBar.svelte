c0c75c807179f341a200df74a6b49a77fb3f3f54
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-11-08 12:20:03 +0100

    Update progress bar styling

--- src/lib/components/progressBar.svelte
+++ src/lib/components/progressBar.svelte
@@ -2,6 +2,6 @@
   export let scrollProgress = 0;
 </script>
 
-<div class="relative mx-4 h-1 w-1/2 overflow-hidden bg-gradient-to-r from-white/20 to-transparent">
+<div class="relative mx-4 h-1 w-1/2 overflow-hidden rounded-full bg-gradient-to-r from-white/20 to-transparent">
   <div class={`absolute bottom-0 left-0 h-full bg-gradient-to-r ${$$props.class}`} style="width: {scrollProgress * 100}%"></div>
 </div>
