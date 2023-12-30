d6b99e0d2c5ddee1389aa1113d517bf9b7d777ac
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-12-31 00:58:11 +0100

    Refactor visionWindow component styles

--- src/lib/components/visionOS/visionWindow.svelte
+++ src/lib/components/visionOS/visionWindow.svelte
@@ -2,7 +2,7 @@
   <div class="absolute -top-4 left-1/2 flex -translate-x-1/2 -translate-y-full justify-center gap-2 rounded-full border border-solid border-white/[0.18] bg-white/25 p-2 drop-shadow backdrop-blur-xl md:-left-4 md:top-1/2 md:-translate-x-full md:-translate-y-1/2 md:flex-col">
     <slot name="buttons" />
   </div>
-  <div class={`relative max-h-[28rem] w-screen overflow-hidden rounded-3xl border border-solid border-white/[0.18] bg-white/25 p-4 drop-shadow-md backdrop-blur-xl will-change-contents md:w-[36rem] lg:max-h-[36rem] lg:w-[72rem] ${$$props.class}`}>
+  <div class={`relative h-[28rem] w-screen overflow-clip rounded-3xl border border-solid border-white/[0.18] bg-white/25 p-4 drop-shadow-md backdrop-blur-xl  will-change-contents md:w-[36rem] lg:h-[36rem] lg:w-[72rem] ${$$props.class}`}>
     <slot />
   </div>
   <div class="relative mt-4 flex w-full justify-center gap-6">
