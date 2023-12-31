aaaa4b94690f35a76d5ffd7ad3504d864dff128e
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-12-31 19:15:41 +0100

    Add tabindex prop to Card component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   export let isDeepDiveOpen = false;
+  export let tabindex = 0;
 
   function handleKeyDown(event: KeyboardEvent) {
     if (event.code === 'Enter' || event.code === 'Space') {
@@ -8,6 +9,6 @@
   }
 </script>
 
-<div class={`relative m-0 cursor-pointer overflow-hidden rounded-none border-y-2  border-neutral-700/40 p-0 transition-all duration-300 md:rounded-3xl md:border-2 lg:hover:scale-105 ${$$props.class}`} on:click={() => (isDeepDiveOpen = true)} on:keydown={(e) => handleKeyDown(e)} tabindex="0" role="button">
+<div class={`relative m-0 cursor-pointer overflow-hidden rounded-none border-y-2  border-neutral-700/40 p-0 transition-all duration-300 md:rounded-3xl md:border-2 lg:hover:scale-105 ${$$props.class}`} on:click={() => (isDeepDiveOpen = true)} on:keydown={(e) => handleKeyDown(e)} {tabindex} role="button" aria-label="Deep dive">
   <slot />
 </div>
