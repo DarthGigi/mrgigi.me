e24d1bbd0c67ab695bc6a066436bfd02a66beef8
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2026-03-26 00:32:55 +0100

    chore: prettier format

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// defaults to rune mode for the project. Can be removed in svelte 6.
-		dynamicCompileOptions: () => ({ runes: true })
-	},
-	kit: { adapter: adapter() }
+  compilerOptions: {
+    // defaults to rune mode for the project. Can be removed in svelte 6.
+    dynamicCompileOptions: () => ({ runes: true })
+  },
+  kit: { adapter: adapter() }
 };
 
 export default config;
