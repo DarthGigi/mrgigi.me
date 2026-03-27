eb46f41e620813a52e89fa74d2ef434a6cf1a4c5
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2026-03-27 01:13:45 +0100

    feat: add contact and projects sections with dynamic content

---
+++ src/lib/sections/contact-section.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { DATA } from "$lib/data/resume";
+  import { Button } from "bits-ui";
+</script>
+
+<div class="relative rounded-xl border p-10">
+  <div
+    class="absolute -top-4 left-1/2 z-10 -translate-x-1/2 rounded-xl border bg-primary px-4 py-1">
+    <span class="text-sm font-medium text-background">Contact</span>
+  </div>
+  <div class="relative flex flex-col items-center gap-4 text-center">
+    <h2 class="text-3xl font-bold tracking-tighter sm:text-5xl">Get in Touch</h2>
+    <p class="mx-auto max-w-lg text-balance text-muted-foreground">
+      <Button.Root
+        href={DATA.contact.social.X.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        class="rounded-sm text-blue-500 underline-offset-4 hover:underline focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:outline-none">
+        Just dm me on Twitter/X
+      </Button.Root>
+      and I'll respond whenever I can.
+    </p>
+    <p class="mx-auto max-w-lg text-xs text-balance text-muted-foreground">
+      Didn't include my email because of the spam bots. I will ignore all soliciting.
+    </p>
+  </div>
+</div>
---
+++ src/lib/sections/projects-section.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import ProjectCard from "$lib/components/project-card.svelte";
+  import { DATA } from "$lib/data/resume";
+</script>
+
+<section id="projects">
+  <div class="flex min-h-0 flex-col gap-y-8">
+    <div class="flex flex-col items-center justify-center gap-y-4">
+      <div class="flex w-full items-center">
+        <div
+          class="h-px flex-1 bg-linear-to-r from-transparent from-5% via-border via-95% to-transparent">
+        </div>
+        <div class="z-10 rounded-xl border bg-primary px-4 py-1">
+          <span class="text-sm font-medium text-background">My Projects</span>
+        </div>
+        <div
+          class="h-px flex-1 bg-linear-to-l from-transparent from-5% via-border via-95% to-transparent">
+        </div>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-y-3">
+        <h2 class="text-3xl font-bold tracking-tighter sm:text-4xl">Check out my projects</h2>
+        <p
+          class="text-center text-balance text-muted-foreground md:text-lg/relaxed lg:text-base/relaxed xl:text-lg/relaxed">
+          I work on a variety of projects, here are a few of them. If you want to see more, check
+          out my GitHub.
+        </p>
+      </div>
+    </div>
+    <div class="mx-auto grid max-w-200 auto-rows-fr grid-cols-1 gap-3 sm:grid-cols-2">
+      {#each DATA.projects as project (project)}
+        <div class="h-full">
+          <ProjectCard {...project} />
+        </div>
+      {/each}
+    </div>
+  </div>
+</section>
