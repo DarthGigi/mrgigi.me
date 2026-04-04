b1da6064cf6651ac978c32ff120312ac5367185a
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2026-04-04 19:59:33 +0200

    feat: add seo stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-  import favicon from "$lib/assets/favicon.svg";
   import AppSidebar from "$lib/components/app-sidebar.svelte";
   import { GlitterCloth } from "$lib/components/motion-core";
+  import { DATA } from "$lib/data/resume";
   import { IsMobile } from "$lib/hooks/is-mobile.svelte";
   import * as Sidebar from "$ui/sidebar";
+  import SvelteSeo from "svelte-seo";
   import "./layout.css";
 
   let { children } = $props();
@@ -11,7 +12,37 @@
   const isMobile = new IsMobile();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<SvelteSeo
+  title={DATA.name}
+  description={DATA.description}
+  canonical={DATA.url}
+  openGraph={{
+    title: DATA.name,
+    description: DATA.description,
+    type: "website",
+    url: DATA.url,
+    // @ts-expect-error It accepts any property
+    image: "/og-image",
+    images: [
+      {
+        url: "/og-image",
+        alt: "Open Graph Image",
+        width: 1200,
+        height: 630,
+        secure_url: `${DATA.url}/og-image`,
+        type: "image/png"
+      }
+    ]
+  }}
+  themeColor="#0a0a0a"
+  twitter={{
+    title: DATA.name,
+    description: DATA.description,
+    image: "/og-image",
+    imageAlt: `${DATA.name} Logo`,
+    card: "summary_large_image",
+    creator: "@DarthGigi"
+  }} />
 
 <GlitterCloth
   class="fixed top-0 left-0 -z-10 h-screen w-screen"
